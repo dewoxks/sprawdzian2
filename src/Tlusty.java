9271c20b9a7d1208fd14752a7d68907d555f4af4
FarciarzYT <tomek.tlustyxd@gmail.com>
2024-09-13 12:14:54 +0200

    first commit

---
+++ src/Tlusty.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Tlusty {
+    public static void main(String[] args) {
+
+        int variably = 45;
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        int number = scanner.nextInt();
+
+        System.out.println(variably + number);
+
+
+
+    }
+}
